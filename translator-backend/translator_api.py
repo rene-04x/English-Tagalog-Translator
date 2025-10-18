a61b86987958baef8f200ea3e024ddbb90b0172d
rene-04x <eneriatelepse@gmail.com>
2025-10-19 00:47:52 +0800

    Update translator_api.py

--- translator-backend/translator_api.py
+++ translator-backend/translator_api.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://rene-04x.github.io/English-Tagalog-Translator/"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
